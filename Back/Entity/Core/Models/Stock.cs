6b350083d4026585f574affdf1d128d51b3ed444
Antonius Atef Ghaly <antonius.a.ghaly@gmail.com>
2022-08-02 14:57:31 +0200

    add product& stock models

--- Back/Entity/Core/Models/Stock.cs
+++ Back/Entity/Core/Models/Stock.cs
@@ -4,15 +4,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entity.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Drawing;
 
 namespace Entity.Core.Models
 {
     public class Stock : EntityId
     {
-        public string Color { get; set; }
-        public Double Price { get; set; }
+        //        public string Color { get; set; }
+        public Color Color { get; set; }
+        public Enums.Size Size { get; set; }
+        public double Price { get; set; }
         public int Quantity { get; set; }
         public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
         public Product Product { get; set; }
+        public ICollection<ProductImage> ProductImages { get; set; }
     }
 }
---
+++ Back/Entity/Enums/Size.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Enums
+{
+    public enum Size
+    {
+        XXXXS,
+        XXXS,
+        XXS,
+        XS,
+        S,
+        M,
+        L,
+        XL,
+        XXL,
+        XXXL,
+        XXXXL,
+        XXXXXL,
+    }
+}
